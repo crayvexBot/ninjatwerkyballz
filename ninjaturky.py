7b3421b398ac641dac5fec7f427dabe3c535260b
crayvexBot <mkaschannel998@gmail.com>
2026-04-18 17:05:36 +0500

    Update ninjaturky.py

--- ninjaturky.py
+++ ninjaturky.py
@@ -190,6 +190,7 @@ def logout():
     session.pop("user", None)
     return redirect(url_for("login"))
 
-# ---------------- RUN ----------------
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
